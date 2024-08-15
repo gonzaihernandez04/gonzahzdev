d5373b8b3b8bcee7e8bbd4cc2be47a24a180dd9c
Gonzalo <hernandezgonzalo5845@gmail.com>
2024-08-15 15:54:29 -0300

    se cambio fuente

--- views/layout.php
+++ views/layout.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta name="description" content="Hola! Mi nombre es Gonzalo Iván Hernández y te invito a ver mi porfolio donde podes ver mi trayecto como Tecnico en Computacion, docente y desarrollador de software, estudiante de Ingenieria en computacion en la Universidad Nacional de Tres de Febrero. Tambien podes ver las opiniones de mis alumnos y contactarme en caso de ser necesario. Te espero! ">
     <link rel="stylesheet" href="/build/css/app.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
     
     <title>gonzahzdev | <?php echo $titulo ?? '';?> </title>
 </head>
